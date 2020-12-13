725b84a7195ba762783a4897b1d336a02fddd485
quynh0911 <phamthihuongquynh.toan@gmail.com>
2020-12-13 13:44:00 +0700

    them model dung cu

---
+++ QuanLyNhanKhau/src/models/DungCuModel.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author M6800
+ */
+public class DungCuModel {
+    private String idThietBi;
+    private String tenThietBi;
+    private int soLuong;
+    private int khaDung;
+    private String moTa;
+
+    public String getIdThietBi() {
+        return idThietBi;
+    }
+
+    public void setIdThietBi(String idThietBi) {
+        this.idThietBi = idThietBi;
+    }
+
+    public String getTenThietBi() {
+        return tenThietBi;
+    }
+
+    public void setTenThietBi(String tenThietBi) {
+        this.tenThietBi = tenThietBi;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public int getKhaDung() {
+        return khaDung;
+    }
+
+    public void setKhaDung(int khaDung) {
+        this.khaDung = khaDung;
+    }
+
+    public String getMoTa() {
+        return moTa;
+    }
+
+    public void setMoTa(String moTa) {
+        this.moTa = moTa;
+    }
+    
+}
